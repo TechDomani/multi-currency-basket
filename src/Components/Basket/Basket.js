cbf837b37e11a41c503851428edb077204b94f2b
Natalie <natalie.dillon@gmail.com>
2021-03-06 16:40:11 +0000

    Update basket numbers

--- src/Components/Basket/Basket.js
+++ src/Components/Basket/Basket.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { useEffect } from "react";
 import BasketItem from "../BasketItem/BasketItem";
 import Currency from "../Currency/Currency";
+import "bootstrap/dist/css/bootstrap.css";
+import './Basket.css'
+import { Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 function Basket() {
 
@@ -11,48 +14,55 @@ function Basket() {
 
     // Static Data
     const currencies = [
-         { currency: "CAD", exchange: 1.26349, symbol: "$"},
-         { currency: "CHF", exchange: 0.915, symbol: "Fr" },
-         { currency: "EUR", exchange: 0.85, symbol: "€" },
-         { currency: "GBP", exchange: 0.716, symbol: "£" },               
-         { currency: "USD", exchange: 1, symbol: "$"}
-     ]; 
+        { currency: "CAD", exchange: 1.26349, symbol: "$" },
+        { currency: "CHF", exchange: 0.915, symbol: "Fr" },
+        { currency: "EUR", exchange: 0.85, symbol: "€" },
+        { currency: "GBP", exchange: 0.716, symbol: "£" },
+        { currency: "USD", exchange: 1, symbol: "$" }
+    ];
 
-     const basketItems = [
+    const basketItems = [
         { usdPrice: 1.00, itemName: "Apples", priceType: "per bag" },
         { usdPrice: 0.80, itemName: "Bread", priceType: "per loaf" },
         { usdPrice: 1.15, itemName: "Milk", priceType: "per bottle" },
         { usdPrice: 0.65, itemName: "Soup", priceType: "per tin" }
-     ];  
-     
-     // Functions
-     const getPriceDisplay = (price) => {
-         console.log(currentCurrency);
-         let currency = currencies.find((c) => c.currency === currentCurrency);
-         console.log(currency);
-         let convertedPrice = Math.round(price*currency.exchange*100)/100;
-         return `${currency.symbol}${convertedPrice}`;
-     }
+    ];
 
-     const updateTotal = (increment) => {
-         let total = basketTotal + increment;
-         setBasketTotal(total);
-     }
-     
-     return (
-       <div>
-            <Currency currencies={currencies.map(c => c.currency)}
-              currentCurrency={currentCurrency}
-              setCurrentCurrency={setCurrentCurrency}>
-            </Currency> 
+    // Functions
+    const getPriceDisplay = (price) => {
+        let currency = currencies.find((c) => c.currency === currentCurrency);
+        let convertedPrice = Math.round(price * currency.exchange * 100) / 100;
+        return `${currency.symbol}${convertedPrice}`;
+    }
+
+    const updateTotal = (increment) => {
+        let total = basketTotal + increment;
+        setBasketTotal(total);
+    }
+
+    return (
+        <Container>
+            <Row className="justify-content-end">
+                <Currency currencies={currencies.map(c => c.currency)}
+                    currentCurrency={currentCurrency}
+                    setCurrentCurrency={setCurrentCurrency}>
+                </Currency>
+            </Row>
             {
-                basketItems.map((b) => <BasketItem key={b.itemName}
-                                                   basketItem={b} 
-                                                   getPriceDisplay={getPriceDisplay} 
-                                                   updateTotal={updateTotal}>
-                                       </BasketItem>)
-            } 
-        </div>
+                basketItems.map((b) => <BasketItem
+                    key={b.itemName}
+                    basketItem={b}
+                    getPriceDisplay={getPriceDisplay}
+                    updateTotal={updateTotal}>
+                </BasketItem>)
+            }
+            <Row className="justify-content-end">
+                <div className="basket-total-container">
+                    <div className="margin-sm">Total:</div>
+                    <div className="margin-sm">{getPriceDisplay(basketTotal)}</div>
+                </div>
+            </Row>
+        </Container>
     );
 }
 
--- src/Components/BasketItem/BasketItem.js
+++ src/Components/BasketItem/BasketItem.js
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './BasketItem.css'
+import "bootstrap/dist/css/bootstrap.css";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 function BasketItem({ basketItem, getPriceDisplay, updateTotal}){
     
     // State
     const [itemNumber, setItemNumber] = useState(0);
 
+    const reduceNumber = () => {
+        if (itemNumber > 0){
+            let newItemNumber = itemNumber - 1;
+            setItemNumber(newItemNumber);
+            updateTotal(-1 * basketItem.usdPrice);
+        }
+    }
+
+    const increaseNumber = () => {
+        // Limit number to 99 as a starting assumption
+        if (itemNumber < 99){
+          let newItemNumber = itemNumber + 1;
+          setItemNumber(newItemNumber);
+          updateTotal(1 * basketItem.usdPrice);
+        }
+    }
+
     return (
-       <div className="basket-item-container">
-           <div className="margin-sm">{basketItem.itemName}</div>
-           <div className="basket-item-control">
-               <button><FontAwesomeIcon icon="minus" /></button>
+       <Row>
+           <Col className="margin-sm">{basketItem.itemName}</Col>
+           <Col className="basket-item-control">
+               <button onClick={reduceNumber}><FontAwesomeIcon icon="minus"/></button>
                <div className="margin-sm">{itemNumber}</div>
-               <button><FontAwesomeIcon icon="plus" /></button>
-           </div>
-           
-           <div className="margin-sm">{getPriceDisplay(basketItem.usdPrice)}</div>
-       </div>
+               <button onClick={increaseNumber}><FontAwesomeIcon icon="plus"/></button>
+           </Col>           
+           <Col className="margin-sm">{getPriceDisplay(basketItem.usdPrice*itemNumber)}</Col>
+       </Row>
     )
 
 }
