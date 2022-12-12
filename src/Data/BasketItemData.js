ec10d0d88088cb0c5a30edd82d3d96d3ea15e3fe
TechDomani <natalie.dillon@gmail.com>
2022-12-12 15:18:15 +0000

    Update granola image

--- src/Data/BasketItemData.js
+++ src/Data/BasketItemData.js
@@ -2,13 +2,14 @@ import soup from '../Images/soup.png'
 import milk from '../Images/milk.png'
 import bread from '../Images/bread.png'
 import apple from '../Images/apple.png'
+import granola from '../Images/granola.png'
 
 const BasketItems = [
     { "usdPrice": 1.00, "itemName": "Apples", "priceType": "per bag", "imageName": apple },
     { "usdPrice": 0.80, "itemName": "Bread", "priceType": "per loaf", "imageName": bread },
     { "usdPrice": 1.15, "itemName": "Milk", "priceType": "per bottle", "imageName": milk },
     { "usdPrice": 0.65, "itemName": "Soup", "priceType": "per tin", "imageName": soup },
-    { "usdPrice": 4.99, "itemName": "Granola", "priceType": "per box", "imageName": soup }
+    { "usdPrice": 4.99, "itemName": "Granola", "priceType": "per box", "imageName": granola }
 ]
 
 export default BasketItems;
